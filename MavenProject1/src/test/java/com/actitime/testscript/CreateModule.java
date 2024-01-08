5ebc56d6fe0e125cd0946a7fed678ebf063e79fd
dell <dell@DESKTOP-3N0O4DU>
2024-01-08 17:52:41 +0530

    qq

---
+++ MavenProject1/src/test/java/com/actitime/testscript/CreateModule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateModule {
+	@Test
+public void CustomerModulr()
+{
+	Reporter.log("CreateModule",true);
+}
+}
